e3623d74380674f513d5f060e9b3b2f7c0eeb4e6
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-03-20 21:58:50 +0200

    CW-4 test with page objects file Rozhkova

---
+++ rozhkovaProj/src/main/java/pages/ParentPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage extends CommonActionsWithElements{
+
+
+    public ParentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+}
