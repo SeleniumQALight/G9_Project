08390f50621ff40d85bd4ff23f3cef0e78a384c0
alenachaban <alena.chaban@synebo.io>
2024-03-23 21:13:16 +0200

    HW3- Test for Invalid Login using created structure and pages

--- chabanProj/src/main/java/pages/HomePage.java
+++ chabanProj/src/main/java/pages/HomePage.java
@@ -8,11 +8,24 @@ public class HomePage extends ParentPage{
     @FindBy(xpath = "//button[contains(text(),'Sign Out')]")
     private WebElement buttonSignOut;
 
+    @FindBy(xpath = "//button[contains(text(),'Sign In')]")
+    private WebElement buttonSignIn;
+
+    @FindBy(xpath = ".//div[contains(text(), 'Invalid username/password.')]")
+    private WebElement popUp;
+
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public boolean isButtonSignOutDisplayed() {
         return isElementDisplayed(buttonSignOut);
+    }
+    public boolean isButtonSignInDisplayed(){
+        return isElementDisplayed(buttonSignIn);
+    }
+    public boolean isPopUpDisplayed(){
+        return isElementDisplayed(popUp);
     }
 }
