fc9173caee8d0f5f4e54efb558c5e3e12b0b2d8d
mrgrt <mrgrtsvl@mrgrtsvl>
2024-03-27 20:45:07 +0100

    CreateNewPostTest Part 2

--- tsivilProj/src/test/java/postTests/CreateNewPostTest.java
+++ tsivilProj/src/test/java/postTests/CreateNewPostTest.java
@@ -1,10 +1,11 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
-
+final String POST_TITLE = "TС_001_tsivil " + Util.getDateAndTimeFormatted();
     @Test
    public void TC_001_createNewPost(){
         pageProvider.getLoginPage()
@@ -12,6 +13,15 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToHomePage()
                 .clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
+                .enterTextIntoInputTitle(POST_TITLE)
+                .enterTextIntoInputBody("body text")
+                //.selectTextInDropdownRoleByVisibleText("Приватне повідомлення")
+                .selectValueInDropdownRole("One Person")
+                .clickOnSaveNewPostButton()
+                .checkIsRedirectToPostPage()
+                .checkIsSuccessMessageDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+
         ;
 
     }
