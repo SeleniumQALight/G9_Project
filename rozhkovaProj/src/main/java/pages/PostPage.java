41b8d1e572681d402f4dab52058238415f91469e
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-03-27 21:45:20 +0200

    CreateNewPostTest part2

---
+++ rozhkovaProj/src/main/java/pages/PostPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PostPage extends ParentPage{
+
+    @FindBy(xpath = ".//div[@class='alert alert-success text-center']")
+    private WebElement successMessage;
+
+    public PostPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public PostPage checkIsRedirectToPostPage() {
+        //TODO check current URL
+        //TODO check some element that must be present at this page
+        return this;
+    }
+
+    public PostPage checkIsSuccessMessageDisplayed() {
+        Assert.assertTrue("Success message is not displayed", isElementDisplayed(successMessage));
+        return this;
+    }
+
+    public PostPage checkTextInSuccessMessage(String expectedMessageText) {
+        String actualText = successMessage.getText();//якщо елемент знайдений по локатору, а не по тексту, то буде працювати на будь-якій мові
+        Assert.assertEquals("Text in message", expectedMessageText, actualText);
+        return this;
+    }
+}
