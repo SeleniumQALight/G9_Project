0b104e25dd9d5f3f1bdbf29a585297d091d9dd88
Roman Semeniuk <semeniukromanser@gmail.com>
2024-03-30 13:25:54 +0200

    HW3-Invalid Login using the created structure and pages

--- semeniukProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ semeniukProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -15,4 +15,20 @@ public class LoginTestWithPageObject extends BaseTest {
 
         Assert.assertTrue("Button Sign Out is not displayed", pageProvider.getHomePage().isButtonSignOutDisplayed());
     }
+
+    @Test
+    public void invalidLogin(){
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin("NotValidLogin");
+        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertTrue("Button Sign In is not visible",
+                pageProvider.getHomePage().isButtonSignInDisplayed());
+        Assert.assertFalse("Button Sing Out is visible",
+                pageProvider.getHomePage().isButtonSignOutDisplayed());
+        Assert.assertTrue("Popup isn't displayed",
+                pageProvider.getHomePage().isPopUpDisplayed());
+
+    }
 }
