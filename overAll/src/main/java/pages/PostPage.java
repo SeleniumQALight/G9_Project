7321e57df82dd8d5e2a40d9733032e903e2a1bac
SeleniumQaLight <automation@qalight.com.ua>
2024-04-03 20:28:16 +0300

    CreateNewPostTest postCondition was added

--- overAll/src/main/java/pages/PostPage.java
+++ overAll/src/main/java/pages/PostPage.java
@@ -11,6 +11,9 @@ public class PostPage extends ParentPage {
     @FindBy(xpath = ".//div[@class='alert alert-success text-center']")
     private WebElement successMessage;
 
+    @FindBy(xpath = ".//button[@class='delete-post-button text-danger']")
+    private WebElement buttonDeletePost;
+
     public PostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -36,5 +39,10 @@ public class PostPage extends ParentPage {
         Assert.assertEquals("Text in message", expectedMessageText
                 , actualText);
         return this;
+    }
+
+    public MyProfilePage clickOnDeleteButton() {
+        clickOnElement(buttonDeletePost);
+        return new MyProfilePage(webDriver);
     }
 }
--- overAll/src/test/java/postTests/CreateNewPostTest.java
+++ overAll/src/test/java/postTests/CreateNewPostTest.java
@@ -2,6 +2,7 @@ package postTests;
 
 import baseTest.BaseTest;
 import libs.Util;
+import org.junit.After;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
@@ -30,6 +31,16 @@ public class CreateNewPostTest extends BaseTest {
                 .getHeaderElement().clickOnMyProfileButton()
                 .checkIsRedirectToMyProfilePage()
                 .checkPostWithTitleIsPresent(POST_TITLE, 1)
+        ;
+    }
+
+    @After
+    public void deletePosts() {
+        pageProvider.getHomePage()
+                .openHomePageAndLoginIfNeeded()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostsTillPresent(POST_TITLE)
         ;
     }
 
