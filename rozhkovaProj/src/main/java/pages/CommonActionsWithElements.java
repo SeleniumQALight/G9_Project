13e2c93f9650ee8cb89951bb55c00cf99892215e
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-04-04 16:48:43 +0300

    fix conflict HW4

--- rozhkovaProj/src/main/java/pages/CommonActionsWithElements.java
+++ rozhkovaProj/src/main/java/pages/CommonActionsWithElements.java
@@ -18,48 +18,49 @@ public class CommonActionsWithElements {
 
     public CommonActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
-        PageFactory.initElements(webDriver,this); //будуть проініціалізовані всі елементи описані в @FindBy
+        PageFactory.initElements(webDriver, this); //будуть проініціалізовані всі елементи описані в @FindBy
         webDriverWait_10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         webDriverWait_15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
     }
 
-    protected void clickOnElement(WebElement webElement){
-        try{
+    protected void clickOnElement(WebElement webElement) {
+        try {
             webDriverWait_10.until(ExpectedConditions.elementToBeClickable(webElement)); //чекаємо поки елемент стане клікабельним (протягом 10 сек
             String elementName = getElementName(webElement);
             webElement.click();
             logger.info(elementName + " Element was clicked");
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
 
-    protected void cleanAndEnterTextIntoElement(WebElement webElement, String text){
-        try{
+    protected void cleanAndEnterTextIntoElement(WebElement webElement, String text) {
+        try {
             webElement.clear();
             webElement.sendKeys(text);
             logger.info(text + " was inputted into element " + getElementName(webElement));
 
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
 
-    protected boolean isElementDisplayed (WebElement webElement){
-        try{
+    protected boolean isElementDisplayed(WebElement webElement) {
+        try {
             boolean state = webElement.isDisplayed();
-            if (state){
+            if (state) {
                 logger.info(getElementName(webElement) + " Element is displayed");
-            }else{
+            } else {
                 logger.info(getElementName(webElement) + " Element is not displayed"); //елемент є на сторінці, але не показаний візуально
             }
             return state;
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.info("Element is not displayed"); //елементу немає на сторінці
             return false;
         }
     }
-    protected boolean isElementDisplayed (WebElement webElement, String elementName){  //якщо треба назву елементу вивести в лог, коли немає назви елементу
+
+    protected boolean isElementDisplayed(WebElement webElement, String elementName) {  //якщо треба назву елементу вивести в лог, коли немає назви елементу
         try {
             boolean state = webElement.isDisplayed();
             if (state) {
@@ -75,12 +76,12 @@ public class CommonActionsWithElements {
     }
 
     // select text in dropdown by visible text
-    protected void selectTextInDropdownByVisibleText(WebElement dropDown, String text){
-        try{
+    protected void selectTextInDropdownByVisibleText(WebElement dropDown, String text) {
+        try {
             Select select = new Select(dropDown);
             select.selectByVisibleText(text);
             logger.info(text + " was selected in dropdown " + getElementName(dropDown));
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
@@ -95,28 +96,28 @@ public class CommonActionsWithElements {
         }
     }
 
-    protected void selectCheckbox(WebElement checkbox){
-        try{
-            if (!checkbox.isSelected()){
+    protected void selectCheckbox(WebElement checkbox) {
+        try {
+            if (!checkbox.isSelected()) {
                 checkbox.click();
                 logger.info("Checkbox was selected");
-            }else{
+            } else {
                 logger.info("Checkbox was already selected");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
 
-    protected void unselectCheckbox(WebElement checkbox){
-        try{
-            if (checkbox.isSelected()){
+    protected void unselectCheckbox(WebElement checkbox) {
+        try {
+            if (checkbox.isSelected()) {
                 checkbox.click();
                 logger.info("Checkbox was unselected");
-            }else{
+            } else {
                 logger.info("Checkbox was already unselected");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
@@ -139,20 +140,15 @@ public class CommonActionsWithElements {
         }
     }
 
-            private void printErrorAndStopTest (Exception e){
-                logger.error("Can not work with element " + e); //іде в консоль і в логфайл
-                Assert.fail("Can not work with element " + e); // іде в репорт
-            }
-        }
     private void printErrorAndStopTest(Exception e) {
         logger.error("Can not work with element " + e); //іде в консоль і в логфайл
         Assert.fail("Can not work with element " + e); // іде в репорт
     }
 
-    private String getElementName(WebElement webElement){
-        try{
+    private String getElementName(WebElement webElement) {
+        try {
             return webElement.getAccessibleName();
-        }catch (Exception e){
+        } catch (Exception e) {
             return "";
         }
     }
