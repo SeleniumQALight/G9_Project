6d715da5b3045439da796a1bad1ced5a4219d80b
Roman Semeniuk <semeniukromanser@gmail.com>
2024-04-08 21:48:40 +0300

    CW9 - errors messages test

---
+++ semeniukProj/src/test/java/registrationTests/ValidationMessagesTest.java
@@ -0,0 +1,20 @@
+package registrationTests;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class ValidationMessagesTest extends BaseTest {
+    final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_EMAIL = "You must provide a valid email address.";
+    final String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final String SEMICOLON = ";";
+
+    @Test
+    public void validationMessagesTest() {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoRegistrationUserNameField("tr")
+        .enterTextIntoRegistrationEmailField("notValidEmail")
+        .enterTextIntoRegistrationPasswordField("notValid")
+                .checkErrorsMessages(ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD);
+    }
+}
