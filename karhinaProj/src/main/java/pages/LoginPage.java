750927798971caf34f66b0b2c158276344e4d518
KarhinaOlena <o.kargina@smile-expo.com>
2024-04-08 21:41:38 +0300

    CW9 errors messages test

--- karhinaProj/src/main/java/pages/LoginPage.java
+++ karhinaProj/src/main/java/pages/LoginPage.java
@@ -1,10 +1,17 @@
 package pages;
 
 import data.TestData;
+import libs.Util;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginPage extends ParentPage{
     @FindBy(xpath = "//button[contains(text(),'Sign In')]") //Ініціалізується в Common ActionsWithElements
@@ -18,6 +25,22 @@ public class LoginPage extends ParentPage{
 
     @FindBy(xpath = "//div[@class='alert alert-danger text-center']")
     private WebElement massageInvalidUsernamePassword;
+
+    @FindBy(xpath = "//*[@id='username-register']") //id="username-register"
+    private WebElement inputUserNameRegistrationForm;
+
+    @FindBy(xpath = "//*[@id='email-register']") //id="email-register"
+    private WebElement inputEmailRegistrationForm;
+
+    @FindBy(xpath = "//*[@id='password-register']") //id="password-register"
+    private WebElement inputPasswordRegistrationForm;
+
+    final static String listErrorsMessagesLocator
+            = "//*[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']";
+
+    @FindBy(xpath = "//*[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+    private List<WebElement> listErrorsMessages;
+
 
 
     public LoginPage(WebDriver webDriver) {
@@ -96,6 +119,49 @@ public class LoginPage extends ParentPage{
 
     public boolean checkIsButtonSignInIsNotDisplayed() {
         return checkElementIsNotDisplayed(buttonSignIn);
+    }
+
+    public LoginPage enterTextIntoRegistrationUserNameField(String userName) {
+        cleanAndEnterTextIntoElement(inputUserNameRegistrationForm, userName);
+        return this;
+    }
+
+        public LoginPage enterTextIntoRegistrationEmailField(String email) {
+        cleanAndEnterTextIntoElement(inputEmailRegistrationForm, email);
+        return this;
+        }
+
+        public LoginPage enterTextIntoRegistrationPasswordField(String password) {
+        cleanAndEnterTextIntoElement(inputPasswordRegistrationForm, password);
+        return this;
+    }
+
+    public LoginPage checkErrorsMessages(String messages) {
+        //error1;error2;error3 -> [error1, error2, error3]
+        String[] expectedErrors = messages.split(";");
+
+        webDriverWait10.until(ExpectedConditions
+                .numberOfElementsToBe(By.xpath(listErrorsMessagesLocator), expectedErrors.length));
+
+        Util.waitABit(1);
+
+        Assert.assertEquals("Number of Messages", expectedErrors.length, listErrorsMessages.size());
+
+        ArrayList<String> actualTextMessages = new ArrayList<>();
+        for (WebElement element : listErrorsMessages) {
+            actualTextMessages.add(element.getText());
+        }
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < expectedErrors.length; i++) {
+            softAssertions.assertThat(actualTextMessages.get(i))
+                            .as("Error " + i)
+                    .isIn(expectedErrors);
+        }
+
+        softAssertions.assertAll();
+
+        return this;
     }
 }
 
