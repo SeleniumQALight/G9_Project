269afdd4833dfc159eb762b104f6143a196d3ce6
alenacha <chabanelena0@gmail.com>
2024-04-08 14:39:28 +0300

    HW5-chaban-05-04

--- chabanProj/src/main/java/pages/elements/HeaderElement.java
+++ chabanProj/src/main/java/pages/elements/HeaderElement.java
@@ -1,11 +1,10 @@
 package pages.elements;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import pages.CommonActionWithElements;
-import pages.CreatePostPage;
-import pages.MyProfilePage;
+import pages.*;
 
 //описуємо елементи які будуть в хедері !залогіненого! юзера
 public class HeaderElement extends CommonActionWithElements {
@@ -19,6 +18,25 @@ public class HeaderElement extends CommonActionWithElements {
 
     @FindBy(xpath = "//*[@class='btn btn-sm btn-success mr-2']")
     private WebElement buttonCreatePost;
+
+    @FindBy(xpath = "//*[@class='text-white mr-2']")
+    private WebElement userName;
+
+    @FindBy(xpath = "//*[@class='text-white mr-2 header-search-icon']")
+    private WebElement searchIcon;
+
+    @FindBy(xpath = "//*[@class='text-white mr-2 header-chat-icon']")
+    private WebElement chatIcon;
+
+    @FindBy(xpath = "//button[contains(text(),'Sign In')]") //ініціалізується в commonActionWithElements
+    private WebElement buttonSignIn;
+
+    @FindBy(xpath = ".//input[@placeholder='Username']")
+    private WebElement inputUserNameLoginForm;
+
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPasswordLoginForm;
+
 
     public HeaderElement(WebDriver webDriver) {
         super(webDriver);
@@ -36,5 +54,44 @@ public class HeaderElement extends CommonActionWithElements {
     public CreatePostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreatePostPage(webDriver);
+    }
+
+    public boolean isCreatePostDisplayed() {
+        return isElementDisplayed(buttonCreatePost);
+    }
+
+    public boolean isMyProfileDisplayed() {
+        return isElementDisplayed(buttonMyProfile);
+    }
+
+    public boolean isUserNameDisplayed() {
+        return isElementDisplayed(userName);
+    }
+
+
+    public HeaderElement assertionsForLoggedInUserElementsDisplayed() {
+        checkElementIsDisplayed(searchIcon);
+        checkElementIsDisplayed(chatIcon);
+        checkElementIsDisplayed(buttonMyProfile);
+        checkElementIsDisplayed(buttonCreatePost);
+        checkElementIsDisplayed(buttonSignOut);
+        return this;
+    }
+
+    public HeaderElement assertionsForLoggedInUserElementsAreNotDisplayed() {
+        checkElementIsNotDisplayed(searchIcon);
+        checkElementIsNotDisplayed(chatIcon);
+        checkElementIsNotDisplayed(buttonMyProfile);
+        checkElementIsNotDisplayed(buttonCreatePost);
+        checkElementIsNotDisplayed(buttonSignOut);
+        return this;
+    }
+
+    public LoginPage getLoginPage() {
+        return new LoginPage(webDriver);
+    }
+    public LoginPage clickOnSignOut() {
+        clickOnElement(buttonSignOut);
+        return new LoginPage(webDriver);
     }
 }
