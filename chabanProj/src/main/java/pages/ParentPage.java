1c31d03884345e331a6aa2897aab6c49a1ca2806
alenacha <chabanelena0@gmail.com>
2024-04-10 21:44:40 +0300

    work with properties

--- chabanProj/src/main/java/pages/ParentPage.java
+++ chabanProj/src/main/java/pages/ParentPage.java
@@ -1,12 +1,14 @@
 package pages;
 
+import libs.ConfigProvider;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 abstract public class ParentPage extends CommonActionWithElements {
 
     String env = System.getProperty("env", "aqa");
-    String baseUrl = String.format("https://%s-complexapp.onrender.com", env);
+   // String baseUrl = String.format("https://%s-complexapp.onrender.com", env);
+    String baseUrl = ConfigProvider.configProperties.base_url().replace("[env]", env);
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
