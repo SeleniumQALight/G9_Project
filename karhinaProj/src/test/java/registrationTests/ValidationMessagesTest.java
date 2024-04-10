7f7565a5b91f1685e914c6f456e32ab7ba02f709
KarhinaOlena <o.kargina@smile-expo.com>
2024-04-10 19:47:24 +0300

    run tests with params

--- karhinaProj/src/test/java/registrationTests/ValidationMessagesTest.java
+++ karhinaProj/src/test/java/registrationTests/ValidationMessagesTest.java
@@ -1,25 +1,37 @@
 package registrationTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+
+@RunWith(JUnitParamsRunner.class)
 public class ValidationMessagesTest extends BaseTest {
     final String ERROR_USERNAME = "Username must be at least 3 characters.";
     final String ERROR_EMAIL = "You must provide a valid email address.";
     final String ERROR_PASSWORD = "Password must be at least 12 characters.";
     final String SEMICOLON = ";";
+
     @Test
+    @Parameters(method = "parametersForTestValidationMessagesTest")
 
-    public void testValidationMessagesTest() {
+    public void testValidationMessagesTest(String userName, String email, String password, String expectedMessages) {
         pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().enterTextIntoRegistrationUserNameField("tr")
-                .enterTextIntoRegistrationEmailField("notValidEmail")
-                .enterTextIntoRegistrationPasswordField("notValid")
-                .checkErrorsMessages(ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD);
+        pageProvider.getLoginPage().enterTextIntoRegistrationUserNameField(userName)
+                .enterTextIntoRegistrationEmailField(email)
+                .enterTextIntoRegistrationPasswordField(password)
+                .checkErrorsMessages(expectedMessages);
+    }
 
-
-
+    public Object[][] parametersForTestValidationMessagesTest() {
+        return new Object[][]{
+                {"taras", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"tr", "tr", "tr", ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD}
+        };
 
     }
+
 
 }
