a99183a0c2bc25aea9e035b00357a376244d20c5
Mariia Buturlym <mbuturlim@gmail.com>
2024-04-15 01:02:04 +0200

    [HW6] Added test with creating new tab and refreshing page

---
+++ buturlimProj/src/test/java/loginTests/LoginInNewTab.java
@@ -0,0 +1,23 @@
+package loginTests;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+
+public class LoginInNewTab extends BaseTest {
+
+    @Test
+    public void loginInNewTab() {
+        pageProvider.getLoginPage().openLoginPageAndFillLoginFormWithValidCred();
+        pageProvider.getHomePage().getHeaderElement().checkIsButtonSignOutVisible();
+        pageProvider.getHomePage().openNewTab();
+        pageProvider.getHomePage().switchToNewTab();
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().getHeaderElement().checkIsButtonSignOutVisible();
+        pageProvider.getHomePage().switchToOldTab();
+        pageProvider.getHomePage().getHeaderElement().checkIsButtonSignOutVisible();
+        pageProvider.getHomePage().switchToNewTab();
+        pageProvider.getHomePage().closeNewTab();
+        pageProvider.getHomePage().getHeaderElement().checkIsButtonSignOutVisible();
+    }
+}
