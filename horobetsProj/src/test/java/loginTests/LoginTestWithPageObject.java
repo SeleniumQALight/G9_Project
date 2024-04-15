9349b26ba679a47f411bea5d4d428865d1412e10
ihorobets <162433469+ihorobets@users.noreply.github.com>
2024-04-15 20:50:06 +0300

    tests with Excel

--- horobetsProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ horobetsProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,13 +1,18 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import libs.ConfigProvider;
+import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoginTestWithPageObject extends BaseTest {
 
     @Test
-    public void validLogin(){
+    public void validLogin() {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto");
         pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
@@ -22,10 +27,23 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getLoginPage().checkIsInputLoginNotDispayed();
         pageProvider.getLoginPage().checkIsInputPasswordNotDispayed();
 
-        }
+    }
 
     @Test
-    public void invalidLogin(){
+    public void validLoginWithExcel() throws IOException {
+        Map<String, String> dataForValidLogin =
+                ExcelDriver.getData(ConfigProvider.configProperties.DATA_FILE(), "validLogOn");
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin(dataForValidLogin.get("login"));
+        pageProvider.getLoginPage().enterTextIntoInputPassword(dataForValidLogin.get("pass"));
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertTrue("Button Sign Out ia not displayed"
+                , pageProvider.getHomePage().getHeaderElement().isButtonSignOutDisplayed());
+    }
+
+    @Test
+    public void invalidLogin() {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage().enterTextIntoInputLogin("qqaauto");
         pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
