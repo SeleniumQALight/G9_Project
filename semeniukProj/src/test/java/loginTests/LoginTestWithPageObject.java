f7a893eae11624c72c0b407a5d8c29544160bd95
Roman Semeniuk <semeniukromanser@gmail.com>
2024-04-15 20:57:21 +0300

    tests with Excel

--- semeniukProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ semeniukProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,8 +1,13 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import libs.ConfigProvider;
+import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoginTestWithPageObject extends BaseTest {
 
@@ -43,4 +48,17 @@ public class LoginTestWithPageObject extends BaseTest {
         Assert.assertTrue("Popup isn't displayed",
                 pageProvider.getHomePage().isPopUpDisplayed());
     }
+
+@Test
+    public void validLoginWithExcel() throws IOException {
+    Map<String, String> dataForValidLogin =
+            ExcelDriver.getData(ConfigProvider.configProperties.DATA_FILE(), "validLogOn");
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin(dataForValidLogin.get("login"));
+        pageProvider.getLoginPage().enterTextIntoInputPassword(dataForValidLogin.get("pass"));
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertTrue("Button Sign Out is not displayed"
+                , pageProvider.getHomePage().getHeaderElement().isButtonSignOutDisplayed());
     }
+}
