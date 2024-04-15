5ac92dd9887351389af7ab13185040f1ff023b07
SeleniumQaLight <automation@qalight.com.ua>
2024-04-15 21:22:14 +0300

    hidden config

--- overAll/src/test/java/loginTests/LoginTestWithPageObject.java
+++ overAll/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,6 +1,7 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import data.TestData;
 import libs.ConfigProvider;
 import libs.ExcelDriver;
 import org.junit.Assert;
@@ -14,8 +15,8 @@ public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin(){
         pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
         pageProvider.getLoginPage().clickOnButtonSignIn();
 
         Assert.assertTrue("Button Sign Out is not displayed"
