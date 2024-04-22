bd2af69a9482aa0c9286b58aa6c808f25212f0a5
KarhinaOlena <o.kargina@smile-expo.com>
2024-04-22 19:43:03 +0300

    Suits

---
+++ karhinaProj/src/test/java/suits/SmokeSuite.java
@@ -0,0 +1,20 @@
+package suits;
+
+
+import catagories.SmokeTestFilter;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import postTests.CreateNewPostTest;
+import registrationTests.ValidationMessagesTest;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(SmokeTestFilter.class)
+@Suite.SuiteClasses({
+        loginTests.LoginTestWithPageObject.class,
+        ValidationMessagesTest.class,
+        CreateNewPostTest.class
+})
+
+public class SmokeSuite {
+}
