2c4ea2fb207d09bbae9b8b018cea0c94d094a648
mrgrt <mrgrtsvl@mrgrtsvl>
2024-04-22 15:10:22 +0200

    tests with exel

--- tsivilProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ tsivilProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,8 +1,13 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import libs.ConfigProvider;
+import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
@@ -13,6 +18,20 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getLoginPage().clickOnButtonSignIn();
 
         Assert.assertTrue("Button Sign Out is not displayed", pageProvider.getHomePage().getHeaderElement().isButtonSignOutDisplayed());
+
+    }
+
+    @Test
+    public void validLoginWithExel() throws IOException {
+        Map<String, String> dataForValidLogin =
+                ExcelDriver.getData(ConfigProvider.configProperties.DATA_FILE(), "validLogOn");
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin(dataForValidLogin.get("login"));
+        pageProvider.getLoginPage().enterTextIntoInputPassword(dataForValidLogin.get("pass"));
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertTrue("Button Sign Out is not displayed",
+                pageProvider.getHomePage().getHeaderElement().isButtonSignOutDisplayed());
 
     }
 
