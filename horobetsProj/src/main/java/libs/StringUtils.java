a872f33e5d3eb6d9d737ff77dd1ac83953a833da
ihorobets <162433469+ihorobets@users.noreply.github.com>
2024-05-02 23:32:11 +0300

    registration tests

---
+++ horobetsProj/src/main/java/libs/StringUtils.java
@@ -0,0 +1,13 @@
+package libs;
+
+import org.junit.Assert;
+
+public class StringUtils {
+
+    public static String deleteSomeSymbols(String line, int numberOfSymbols) {
+        if (line.length() <= numberOfSymbols) {
+            Assert.fail("Line is too short");
+        }
+        return line.substring(0, line.length() - numberOfSymbols);
+    }
+}
---
+++ horobetsProj/src/test/java/registrationTests/ValidationMessagesWithoutSomeFieldsTest.java
@@ -0,0 +1,48 @@
+package registrationTests;
+
+import baseTest.BaseTest;
+import categories.SmokeTestFilter;
+import data.User;
+import io.qameta.allure.Step;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static data.RegistrationValidationMessages.*;
+
+
+@RunWith(JUnitParamsRunner.class)
+@Category(SmokeTestFilter.class)
+public class ValidationMessagesWithoutSomeFieldsTest extends BaseTest {
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    @Parameters(method = "parametersForValidationMessagesTest")
+    @Step
+    public void TC021_validationMessagesTest(String caseName, User user, String expectedMessages) {
+        logger.info(caseName);
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage()
+                .enterRegistrationDataIfNotNull(user.getUserName(), user.getEmail(), user.getPassword())
+                .clickOnSignUpButton();
+        pageProvider.getLoginPage()
+                .checkErrorsMessages(expectedMessages);
+    }
+
+    @Step
+    public Object[][] parametersForValidationMessagesTest() {
+        return new Object[][]{
+                {"Fill only Email and Password", new User("TC021").updateUserName(null)
+                        , ERROR_USERNAME},
+                {"Fill only UserName and Password", new User("TC021").updateEmail(null)
+                        , ERROR_EMAIL},
+                {"Fill only UserName and Email ", new User("TC021").updatePassword(null)
+                        , ERROR_PASSWORD},
+        };
+    }
+
+
+}
