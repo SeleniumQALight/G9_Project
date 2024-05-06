7f97f86e9c49b40cbcb61b6329a9316a1044cd75
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-05-06 21:56:39 +0300

    First APITEST: getAllPostsForUsers(given,when,then)

---
+++ rozhkovaProj/src/main/java/api/EndPoints.java
@@ -0,0 +1,8 @@
+package api;
+
+public interface EndPoints { //інтерфейс тому що використовуємо фічі рест асуред
+    String BASE_URL = "https://aqa-complexapp.onrender.com";
+    String POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";
+
+
+}
