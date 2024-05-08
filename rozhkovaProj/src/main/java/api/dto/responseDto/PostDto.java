a8520a42d5aa0315be6f1c81c1a5f9e5bf78866b
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-05-08 21:38:04 +0300

    work with DTO

---
+++ rozhkovaProj/src/main/java/api/dto/responseDto/PostDto.java
@@ -0,0 +1,109 @@
+package api.dto.responseDto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostDto {
+    //всі поля ссилочні мають бути, щоб якщо поле не прийде, то було налл
+    //розібрати наш респонс, як ми описуємо нижче. замапити json на java обєкт
+    @JsonProperty("_id")//щоб було поле без _ у назві методу - в json
+    private String id;//це вже назва в джаві, для методів з якими будемо працювати в коді
+    private String title;
+    private String body;
+    @JsonProperty("select1")//щоб було поле без 1
+    private String select;
+    private String uniquePost;
+    private String createdDate;
+
+    private AuthorDto author;
+    private Boolean isVisitorOwner;
+
+    public PostDto() { //дефолтний конструктор
+    }
+
+    public PostDto(String title, String body, String select, String uniquePost, AuthorDto author, Boolean isVisitorOwner) {
+        this.title = title;
+        this.body = body;
+        this.select = select;
+        this.uniquePost = uniquePost;
+        this.author = author;
+        this.isVisitorOwner = isVisitorOwner;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSelect() {
+        return select;
+    }
+
+    public void setSelect(String select) {
+        this.select = select;
+    }
+
+    public String getUniquePost() {
+        return uniquePost;
+    }
+
+    public void setUniquePost(String uniquePost) {
+        this.uniquePost = uniquePost;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    public Boolean getIsVisitorOwner() {
+        return isVisitorOwner;
+    }
+
+    public void setIsVisitorOwner(Boolean visitorOwner) {
+        isVisitorOwner = visitorOwner;
+    }
+
+    @Override
+    public String toString() {//відображення обєкта в консольці
+        return "PostDto{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", select='" + select + '\'' +
+                ", uniquePost='" + uniquePost + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", author=" + author +
+                ", isVisitorOwner=" + isVisitorOwner +
+                '}';
+    }
+}
