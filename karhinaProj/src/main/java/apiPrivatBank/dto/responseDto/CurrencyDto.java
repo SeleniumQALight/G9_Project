7051e6c247a83b72fe88c6db52389ca19da6ab9b
KarhinaOlena <o.kargina@smile-expo.com>
2024-05-10 22:10:07 +0300

    HW1 API Get exchangeRate PrivatBank

---
+++ karhinaProj/src/main/java/apiPrivatBank/dto/responseDto/CurrencyDto.java
@@ -0,0 +1,82 @@
+package apiPrivatBank.dto.responseDto;
+
+public class CurrencyDto {
+    private String baseCurrency;
+    private String currency;
+    private Double saleRateNB;
+    private Double purchaseRateNB;
+    private Double saleRate;
+    private Double purchaseRate;
+
+    public CurrencyDto() {
+    }
+
+
+    public CurrencyDto(String baseCurrency, String currency) {
+        this.baseCurrency = baseCurrency;
+        this.currency = currency;
+
+    }
+
+    // Геттеры и сеттеры для всех полей
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getSaleRateNB() {
+        return saleRateNB;
+    }
+
+    public void setSaleRateNB(double saleRateNB) {
+        this.saleRateNB = saleRateNB;
+    }
+
+    public double getPurchaseRateNB() {
+        return purchaseRateNB;
+    }
+
+    public void setPurchaseRateNB(double purchaseRateNB) {
+        this.purchaseRateNB = purchaseRateNB;
+    }
+
+    public double getSaleRate() {
+        return saleRate;
+    }
+
+    public void setSaleRate(double saleRate) {
+        this.saleRate = saleRate;
+    }
+
+    public double getPurchaseRate() {
+        return purchaseRate;
+    }
+
+    public void setPurchaseRate(double purchaseRate) {
+        this.purchaseRate = purchaseRate;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyDto{" +
+                "baseCurrency='" + baseCurrency + '\'' +
+                ", currency='" + currency + '\'' +
+                ", saleRateNB=" + saleRateNB +
+                ", purchaseRateNB=" + purchaseRateNB +
+                ", saleRate=" + saleRate +
+                ", purchaseRate=" + purchaseRate +
+                "}\n";
+    }
+}
