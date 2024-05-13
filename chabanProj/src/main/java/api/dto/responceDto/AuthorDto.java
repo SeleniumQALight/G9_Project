80f4f7d3ee26eeb666a65bb34f77f29d5fd9cd18
alenacha <chabanelena0@gmail.com>
2024-05-13 19:55:12 +0300

    Lombok

--- chabanProj/src/main/java/api/dto/responceDto/AuthorDto.java
+++ chabanProj/src/main/java/api/dto/responceDto/AuthorDto.java
@@ -1,37 +1,46 @@
 package api.dto.responceDto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class AuthorDto {
     private String username;
     private String avatar;
 
-public AuthorDto() {
-    }
+//    public AuthorDto() {
+//    }
 
-    public AuthorDto(String username) {
-        this.username = username;
-    }
+//    public AuthorDto(String username) {
+//        this.username = username;
+//    }
 
-    public String getUsername() {
-        return username;
-    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getAvatar() {
+//        return avatar;
+//    }
+//
+//    public void setAvatar(String avatar) {
+//        this.avatar = avatar;
+//    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthorDto{" +
-                "username='" + username + '\'' +
-                ", avatar='" + avatar + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "AuthorDto{" +
+//                "username='" + username + '\'' +
+//                ", avatar='" + avatar + '\'' +
+//                '}';
+//    }
 }
