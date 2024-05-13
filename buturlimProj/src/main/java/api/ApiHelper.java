efa7e314fffdc5bb4d88c00a7af7ad6970b54fdc
Mariia Buturlym <mbuturlim@gmail.com>
2024-05-13 20:01:23 +0200

    Response with JSON path

--- buturlimProj/src/main/java/api/ApiHelper.java
+++ buturlimProj/src/main/java/api/ApiHelper.java
@@ -2,13 +2,13 @@ package api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiHelper {
 
     public ValidatableResponse getAllPostsByUserRequest(String userName, int statusCode) {
-
         return given()
                 .contentType(ContentType.JSON)
                 .log().all()
@@ -19,5 +19,9 @@ public class ApiHelper {
                 .statusCode(statusCode);
     }
 
+    public ValidatableResponse getAllPostsByUserRequest(String userName) {
+        return getAllPostsByUserRequest(userName, HttpStatus.SC_OK);
+
+    }
 
 }
