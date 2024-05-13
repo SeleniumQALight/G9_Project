bff67092c40bae0485e88dee84d631822d23f9d3
Roman Semeniuk <semeniukromanser@gmail.com>
2024-05-13 21:44:17 +0300

    CW4.3 - json schema

--- semeniukProj/src/test/java/apiTests/ApiTests.java
+++ semeniukProj/src/test/java/apiTests/ApiTests.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
@@ -122,6 +123,13 @@ public class ApiTests {
         }
 
         softAssertions.assertAll();
+    }
+
+
+    @Test
+    public void getAllPostsByUserSchema(){
+        apiHelper.getAllPostsByUserRequest(USER_NAME)
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
     }
 
 }
