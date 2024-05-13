ac9fb709fe3e79a309e5b38ba8d59e59f9bf059d
alenacha <chabanelena0@gmail.com>
2024-05-13 21:01:38 +0300

    Json Path

--- chabanProj/src/main/java/api/ApiHelper.java
+++ chabanProj/src/main/java/api/ApiHelper.java
@@ -2,6 +2,7 @@ package api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.apache.hc.core5.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,5 +17,8 @@ public class ApiHelper {
                 .log().all()
                 .statusCode(statusCode);
 
+    }
+    public ValidatableResponse getAllPostsByUserRequest(String userName){
+        return getAllPostsByUserRequest(userName, HttpStatus.SC_OK);
     }
 }
