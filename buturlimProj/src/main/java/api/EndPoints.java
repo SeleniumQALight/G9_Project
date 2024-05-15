c53ce9481c45972763880c6a526e0186847151bf
Mariia Buturlym <mbuturlim@gmail.com>
2024-05-15 21:08:37 +0200

    Created post by api

--- buturlimProj/src/main/java/api/EndPoints.java
+++ buturlimProj/src/main/java/api/EndPoints.java
@@ -1,7 +1,12 @@
 package api;
 
+
+import java.net.URI;
+
 public interface EndPoints {
     String BASE_URL = "https://aqa-complexapp.onrender.com";
     String POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";
-
+    String LOGIN = BASE_URL+"/api/login";
+    String CREATE_POST = BASE_URL + "/api/create-post";
+    String DELETE_POST = BASE_URL + "/api/post/{0}";
 }
