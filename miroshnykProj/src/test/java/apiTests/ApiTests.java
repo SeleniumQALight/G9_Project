fed309092ec712acf0e4cf70f453280707b8b145
Miroshnyk <mixamm55@gmail.com>
2024-05-15 16:59:15 +0300

    json schema

--- miroshnykProj/src/test/java/apiTests/ApiTests.java
+++ miroshnykProj/src/test/java/apiTests/ApiTests.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
@@ -124,6 +125,12 @@ public class ApiTests {
 
         softAssertions.assertAll();
 
+    }
+
+    @Test
+    public void getAllPostsByUserSchema(){
+        apiHelper.getAllPostsByUserRequest(USER_NAME)
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
     }
 
 }
