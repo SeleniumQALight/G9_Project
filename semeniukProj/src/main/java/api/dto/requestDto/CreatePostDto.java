192261c601ceceb398ab99a09d644285d4a88bb6
Roman Semeniuk <semeniukromanser@gmail.com>
2024-05-15 22:18:15 +0300

    CW 4.4 - Create post by API

---
+++ semeniukProj/src/main/java/api/dto/requestDto/CreatePostDto.java
@@ -0,0 +1,17 @@
+package api.dto.requestDto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class CreatePostDto {
+    private String title;
+    private String body;
+    private String select1;
+    private String uniquePost;
+    private String token;
+}
