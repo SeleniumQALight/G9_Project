fb9eac7fb8efb3e7dcbc8641175f5afbf25cfdab
Miroshnyk <mixamm55@gmail.com>
2024-05-15 15:58:42 +0300

    Response as String

--- miroshnykProj/src/test/java/apiTests/ApiTests.java
+++ miroshnykProj/src/test/java/apiTests/ApiTests.java
@@ -1,5 +1,6 @@
 package apiTests;
 
+import api.ApiHelper;
 import api.EndPoints;
 import api.dto.responsDto.AuthorDto;
 import api.dto.responsDto.PostsDto;
@@ -17,6 +18,7 @@ import static org.hamcrest.CoreMatchers.everyItem;
 public class ApiTests {
     final String USER_NAME = "autoapi";
     private Logger logger = Logger.getLogger(getClass());
+    private ApiHelper apiHelper = new ApiHelper();
     @Test
     public void getAllPostsForUsers(){
 
@@ -77,7 +79,23 @@ public class ApiTests {
 
         softAssertions.assertAll();
 
-
-
     }
+
+    @Test
+
+    public void getAllPostsByUserNegative(){
+        final String NOT_VALID_USER_NAME = "NotValidUser";
+        // method 3 - Response as String
+        String actualResponse = apiHelper
+                .getAllPostsByUserRequest(NOT_VALID_USER_NAME, 400)
+                .extract().response().body().asString();
+
+        Assert.assertEquals(
+                ""
+                , "\"Sorry, invalid user requested. Wrong username - "+NOT_VALID_USER_NAME+
+                        " or there is no posts. Exception is undefined\""
+                , actualResponse
+        );
+    }
+
 }
