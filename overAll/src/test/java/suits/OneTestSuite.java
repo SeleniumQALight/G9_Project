607b34b10fe6b12a2d3dcef46a69ae9256d5af0f
SeleniumQaLight <automation@qalight.com.ua>
2024-05-20 13:32:34 +0300

    OneTestSuite

---
+++ overAll/src/test/java/suits/OneTestSuite.java
@@ -0,0 +1,14 @@
+package suits;
+
+import loginTests.LoginTestWithPageObject;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoginTestWithPageObject.class
+
+
+})
+public class OneTestSuite {
+}
