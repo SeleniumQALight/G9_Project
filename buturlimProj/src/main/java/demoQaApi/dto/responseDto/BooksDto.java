af658c10d7d188e3a164552a14a52d6f46b2dc08
Mariia Buturlym <mbuturlim@gmail.com>
2024-05-21 21:31:25 +0200

    [HW] End to end api test DemoQA

---
+++ buturlimProj/src/main/java/demoQaApi/dto/responseDto/BooksDto.java
@@ -0,0 +1,20 @@
+package demoQaApi.dto.responseDto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Builder
+public class BooksDto {
+    private String isbn;
+    private String title;
+    private String subTitle;
+    private String author;
+    private String publish_date;
+    private String publisher;
+    private Integer pages;
+    private String description;
+    private String website;
+}
