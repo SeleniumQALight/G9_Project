63c49f9a61472804f6ed9f0919bab73925f85931
Roman Semeniuk <semeniukromanser@gmail.com>
2024-05-24 19:31:13 +0300

    HW4.2 - getAndAddBooksToUser

--- semeniukProj/src/main/java/api/EndPoints.java
+++ semeniukProj/src/main/java/api/EndPoints.java
@@ -6,4 +6,8 @@ public interface EndPoints {
     String LOGIN = BASE_URL + "/api/login";
     String CREATE_POST = BASE_URL + "/api/create-post";
     String DELETE_POST = BASE_URL + "/api/post/{0}";
+    String DEMO_QA_BASE_URL = "https://demoqa.com";
+    String DEMO_QA_BOOKS = DEMO_QA_BASE_URL + "/BookStore/v1/Books";
+    String DEMO_QA_USER = DEMO_QA_BASE_URL + "/Account/v1/User/{0}";
+    String DEMO_QA_LOGIN = DEMO_QA_BASE_URL + "/Account/v1/Login";
 }
