a90b57ada1d212fb38284fbeee812bc1d2e2440d
ihorobets <162433469+ihorobets@users.noreply.github.com>
2024-05-27 11:08:52 +0300

    PrivatBank Test

---
+++ horobetsProj/src/main/java/apiPrivatBank/EndPointsPrivatBank.java
@@ -0,0 +1,8 @@
+package apiPrivatBank;
+
+public interface EndPointsPrivatBank {
+
+    String BASE_URL = "https://api.privatbank.ua";
+    String EXCHANGE_RATES = BASE_URL + "/p24api/exchange_rates";
+
+}
---
+++ horobetsProj/src/main/java/apiPrivatBank/dto/responseDto/CurrencyDto.java
@@ -0,0 +1,80 @@
+package apiPrivatBank.dto.responseDto;
+
+public class CurrencyDto {
+
+    private String baseCurrency;
+    private String currency;
+    private Double saleRateNB;
+    private Double purchaseRateNB;
+    private Double saleRate;
+    private Double purchaseRate;
+
+    public  CurrencyDto(){
+
+    }
+
+    public CurrencyDto(String baseCurrency, String currency) {
+        this.baseCurrency = baseCurrency;
+        this.currency = currency;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Double getSaleRateNB() {
+        return saleRateNB;
+    }
+
+    public void setSaleRateNB(Double saleRateNB) {
+        this.saleRateNB = saleRateNB;
+    }
+
+    public Double getPurchaseRateNB() {
+        return purchaseRateNB;
+    }
+
+    public void setPurchaseRateNB(Double purchaseRateNB) {
+        this.purchaseRateNB = purchaseRateNB;
+    }
+
+    public Double getSaleRate() {
+        return saleRate;
+    }
+
+    public void setSaleRate(Double saleRate) {
+        this.saleRate = saleRate;
+    }
+
+    public Double getPurchaseRate() {
+        return purchaseRate;
+    }
+
+    public void setPurchaseRate(Double purchaseRate) {
+        this.purchaseRate = purchaseRate;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyDto{" +
+                "baseCurrency='" + baseCurrency + '\'' +
+                ", currency='" + currency + '\'' +
+                ", saleRateNB=" + saleRateNB +
+                ", purchaseRateNB=" + purchaseRateNB +
+                ", saleRate=" + saleRate +
+                ", purchaseRate=" + purchaseRate +
+                '}';
+    }
+}
