2c4d99b3304a5002510a3e19b69cab8ebdd697d1
Mariia Buturlym <mbuturlim@gmail.com>
2024-05-27 20:52:56 +0200

    [CW] bdd test R002 + alure

--- buturlimProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
+++ buturlimProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -2,7 +2,9 @@ package bdd.stepDefinitions;
 
 import bdd.helpers.WebDriverHelper;
 import data.TestData;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginPageSteps extends MainSteps {
@@ -20,5 +22,25 @@ public class LoginPageSteps extends MainSteps {
         pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
         pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
         pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+
+    @When("I enter {string} into input Login in Login page")
+    public void iEnterIntoInputLoginInLoginPage(String login) {
+        pageProvider.getLoginPage().enterTextIntoInputLogin(login);
+    }
+
+    @When("I enter {string} into input Password in Login page")
+    public void iEnterIntoInputPasswordInLoginPage(String password) {
+        pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+    }
+
+    @When("I click on Sign In button in Login page")
+    public void i_click_on_sign_in_button_in_login_page() {
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+
+    @Then("I see alert message with text {string}")
+    public void iSeeAlertMessageWithTextInvalidUsernamePassword(String message) {
+        pageProvider.getLoginPage().checkTextInAlertMesage(message);
     }
 }
--- buturlimProj/src/test/resources/features/login/login.feature
+++ buturlimProj/src/test/resources/features/login/login.feature
@@ -5,3 +5,17 @@ Feature: Login Feature
     Given I open Login page
     When I login with valid credentials
     Then I see avatar on Home page
+
+  @R002
+  Scenario Outline: R002 Invalid Login
+    Given I open Login page
+    When I enter '<login>' into input Login in Login page
+    And I enter '<password>' into input Password in Login page
+    And I click on Sign In button in Login page
+    Then I see alert message with text 'Invalid username/password.'
+
+    Examples:
+      | login         | password         |
+      | qaauto        | notvalidpassword |
+      | notvalidlogin | 123456qwerty     |
+
