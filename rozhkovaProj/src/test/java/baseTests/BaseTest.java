24bd80ec124bc13377f72a8fbf1d75bb8401e70f
Rozhkova <tetiana.rozhkova.o@gmail.com>
2024-06-20 18:20:33 +0300

    SeleniumGrid+Selenide

--- rozhkovaProj/src/test/java/baseTests/BaseTest.java
+++ rozhkovaProj/src/test/java/baseTests/BaseTest.java
@@ -14,13 +14,18 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
 import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.ArrayList;
 
@@ -75,6 +80,22 @@ public class BaseTest {
         } else if ("edge".equalsIgnoreCase(browserFromProperty)) {
             WebDriverManager.edgedriver().setup();
             webDriver = new EdgeDriver();
+        } else if ("remote".equals(browserFromProperty)) { //щоб відправити тест на селеніум град
+            logger.info("Remote browser");
+            WebDriverManager.chromedriver().setup();//скачає exe файл
+            DesiredCapabilities cap = new DesiredCapabilities();//налаштування браузера, наприклад налаштування дефолтної папки для завантаження файлів
+            cap.setBrowserName("chrome");
+            ChromeOptions chromeOptions = new ChromeOptions();//налаштування браузера, наприклад відключити друк на принтер
+            chromeOptions.merge(cap);
+            try {
+                webDriver = new RemoteWebDriver(
+                        new URL("http://localhost:4444/wd/hub"),//адреса, де запущений селеніум грід
+                        chromeOptions);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }else {
+            throw new IllegalArgumentException("Browser " + browserFromProperty + " is not supported");
         }
         return webDriver; //відкривання браузера Chrome
     }
